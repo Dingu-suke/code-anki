206e2b3fce415d37b35d98b495490bf2d7590e95
Dingu-suke <e2310000rubik@gmail.com>
2024-07-12 02:12:09 +0900

    resolvecss_conflict

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,8 @@
 //= link_tree ../images
 //= link_tree ../builds
+
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link application.css
+//= link easymde/dist/easymde.min.css
