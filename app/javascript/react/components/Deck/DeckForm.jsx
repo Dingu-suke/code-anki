11ced5ebc10661b50b76ed40da581b236021db60
Dingu-suke <e2310000rubik@gmail.com>
2024-08-27 23:47:25 +0900

    [✨]コンポーネント化

--- app/javascript/react/components/Deck/DeckForm.jsx
+++ app/javascript/react/components/Deck/DeckForm.jsx
@@ -7,6 +7,7 @@ import { Drill } from '../Drill/Drill';
 import { PreviewCard } from '../Drill/PreviewCard';
 import { useCardNavigation } from '../../hooks/useCardNavigation';
 import { PreviewCardList } from './PreviewCardList';
+import { SelectCardIndex } from './SelectCardIndex';
 
 export const DeckForm = () => {
   const { cards, setCards, isLoading, setIsLoading } = useCards();
@@ -78,77 +79,6 @@ export const DeckForm = () => {
     return <div>Loading...</div>
   }
 
-
-  const  CardIndex = (
-    <div className="container mx-auto">
-      <h1 className="text-2xl font-bold mb-2 text-orange-400 font-courier">{/* あなたのカード */}</h1>
-        <input
-          type="text"
-          id="searchCards"
-          name="searchCards"
-          placeholder="カードを検索"
-          value={searchTerm}
-          onChange={(e) => { setSearchTerm(e.target.value)}}
-          className="w-full p-2 mb-4 border rounded bg-gray-700 focus:outline-none focus:border-2 focus:border-blue-800 border-blue-900 text-cyan-100"
-        />
-        <div className="col-span-4">
-          <div className="border border-slate-600 bg-stone-950 text-cyan-50 rounded overflow-hidden">
-            <div className="p-4 h-[calc(60vh-2rem)] overflow-auto">
-
-              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4" >
-                <div className="border border-dashed hover:border-solid border-pink-400 hover:border-pink-400 text-pink-400 p-4 rounded shadow  bg-slate-950 flex items-center">
-                  <h2 className='text-xl font-semibold'>+ new card</h2>
-                </div>
-                {filteredCards.map((card) => (
-                  <div
-                  key={card.id}
-                  className={`border hover:border-cyan-300 p-4 rounded shadow hover:bg-indigo-900 
-                  ${card === selectedCard ? 'bg-indigo-900 border-green-500' : 'bg-indigo-950 border-cyan-600' }`}
-                  onClick={(event) => handleCardClick(event, card)}
-                  >
-                    <h2 className='text-xl font-semibold '>
-                      <div className="grid grid-cols-2">
-                        <div className="text-cyan-100 flex items-center">
-                          {card.title}
-                        <div>
-                      </div>
-                      </div>
-                      <div className="justify-self-end checkbox-container" >
-                        <input 
-                          type="checkbox"
-                          className="checkbox checkbox-lg checkbox-secondary hover:bg-pink-900 m-1 flex items-center"
-                          // checked={sedCards.some((c) => c.id === card.id)}
-                          onClick={() => handleCheckboxClick(card)}
-                          checked={card.isSelected} // カードの選択状態を反映
-                          />
-                      </div>
-                    </div>
-                    </h2>
-                  </div>
-                ))}
-              </div>
-                <br/><br/><br/><br/>
-                <br/><br/><br/><br/>
-                <br/><br/><br/><br/>
-                <br/><br/><br/><br/>
-                <br/><br/><br/><br/>
-              {isWindowOpen && (
-                <ResponsiveWindow
-                  title={`${selectedCard.title}`}
-                  initialPosition={{ x: 100, y: 500 }}
-                  initialSize={{ width: 600, height: 450 }}
-                  onClose={closeWindow}
-                >
-                  <CheckCard selectedCard={selectedCard}/>
-                </ResponsiveWindow>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>    
-  );
-  
-
   return (
     <div>
       <PreviewCardList checkedCards={checkedCards} setCheckedCards={setCheckedCards} previewCard={previewCard} setPreviewCard={setPreviewCard} />
@@ -179,7 +109,15 @@ export const DeckForm = () => {
             id="cardIndex-panel"
             className={`p-6 ${activeTab === 'cardIndex' ? '' : 'hidden'} text-white`}
           >
-            {CardIndex}
+            <SelectCardIndex 
+              selectedCard={selectedCard}
+              searchTerm={searchTerm}
+              filteredCards={filteredCards}
+              isWindowOpen={isWindowOpen}
+              closeWindow={closeWindow}
+              handleCardClick={handleCardClick}
+              CheckCard={CheckCard}
+              handleCheckboxClick={handleCheckboxClick}/>
           </div>
           <div
             role="tabpanel"
