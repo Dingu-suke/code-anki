214822e41c580187b00a45a0bab0b12b918396bb
Dingu-suke <e2310000rubik@gmail.com>
2024-11-04 05:35:25 +0900

    環境変数編集

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,25 @@
+# config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
-    provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'] # 1
+    provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
   else
-    provider :github, # 2
-      Rails.application.credentials.github[:client_id],
-      Rails.application.credentials.github[:client_secret]
-  end 
+    # 環境変数で現在のドメインを指定
+    # ミドルウェア層( コントローラより前段階 )
+    proc = lambda do |env|
+      request = Rack::Request.new(env)
+      
+      credentials = case request.host
+                  when ENV[HEROKU]
+                    Rails.application.credentials.github.heroku
+                  when ENV[WWWDOMAIN]
+                    Rails.application.credentials.github.www_domain
+                  when ENV[DOMAIN]
+                    Rails.application.credentials.github.domain
+                  end
+
+      [credentials[:client_id], credentials[:client_secret]]
+    end
+
+    provider :github, proc
+  end
 end
