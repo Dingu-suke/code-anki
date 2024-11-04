55f12979f17b9ad276b0359fe0136f6f8d54c12c
Dingu-suke <e2310000rubik@gmail.com>
2024-11-04 11:15:26 +0900

    omniauth_lambda

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,33 +2,34 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
     provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'] # 1
   else
-    client_id = -> (env) {
-      request = Rack::Request.new(env)
-      case request.host
-      when ENV['HEROKU']
-        Rails.application.credentials.github[:heroku][:client_id]
-      when ENV['WWWDOMAIN']
-        Rails.application.credentials.github[:www_domain][:client_id]
-      when ENV['DOMAIN']
-        Rails.application.credentials.github[:domain][:client_id]
-      else
-        Rails.application.credentials.github[:heroku][:client_id]
-      end
-    }.call.to_s
-
-    client_secret = -> (env) {
-      request = Rack::Request.new(env)
-      case request.host
-      when ENV['HEROKU']
-        Rails.application.credentials.github[:heroku][:client_secret]
-      when ENV['WWWDOMAIN']
-        Rails.application.credentials.github[:www_domain][:client_secret]
-      when ENV['DOMAIN']
-        Rails.application.credentials.github[:domain][:client_secret]
-      else
-        Rails.application.credentials.github[:heroku][:client_secret]
-      end
-    }.call.to_s    
-    provider :github, client_id, client_secret
+    provider :github,
+      ->(env) { 
+        host = Rack::Request.new(env).host
+        key = case host
+              when 'code-anki-0ff3aa07f2cc.herokuapp.com'
+                :heroku
+              when 'www.code-anki.com'
+                :www_domain
+              when 'code-anki.com'
+                :domain
+              else
+                :heroku
+              end
+        Rails.application.credentials.github[key][:client_id]
+      },
+      ->(env) {
+        host = Rack::Request.new(env).host
+        key = case host
+              when 'code-anki-0ff3aa07f2cc.herokuapp.com'
+                :heroku
+              when 'www.code-anki.com'
+                :www_domain
+              when 'code-anki.com'
+                :domain
+              else
+                :heroku
+              end
+        Rails.application.credentials.github[key][:client_secret]
+      }
   end
 end
