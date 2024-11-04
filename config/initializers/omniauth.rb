57483a80218cbf0288a21380884f8f86ac2d8353
Dingu-suke <e2310000rubik@gmail.com>
2024-11-04 11:02:16 +0900

    omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,40 +1,34 @@
-# config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
-    provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
+    provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'] # 1
   else
-    # client_idとclient_secretを個別に取得するlambdaを用意
-    client_id = lambda { |env|
+    client_id = -> (env) {
       request = Rack::Request.new(env)
-      creds = case request.host
-              when ENV['HEROKU']
-                Rails.application.credentials.github.heroku
-              when ENV['WWWDOMAIN']
-                Rails.application.credentials.github.www_domain
-              when ENV['DOMAIN']
-                Rails.application.credentials.github.domain
-              end
-      Rails.logger.info "Host: #{request.host}"
-      creds&.client_id
-    }
+      case request.host
+      when ENV['HEROKU']
+        Rails.application.credentials.github[:heroku][:client_id]
+      when ENV['WWWDOMAIN']
+        Rails.application.credentials.github[:www_domain][:client_id]
+      when ENV['DOMAIN']
+        Rails.application.credentials.github[:domain][:client_id]
+      else
+        Rails.application.credentials.github[:heroku][:client_id]
+      end
+    }.call.to_s
 
-    client_secret = lambda { |env|
+    client_secret = -> (env) {
       request = Rack::Request.new(env)
-      creds = case request.host
-              when ENV['HEROKU']
-                Rails.application.credentials.github.heroku
-              when ENV['WWWDOMAIN']
-                Rails.application.credentials.github.www_domain
-              when ENV['DOMAIN']
-                Rails.application.credentials.github.domain
-              end
-      creds&.client_secret
-    }
-
-    # 個別のlambdaを引数として渡す
+      case request.host
+      when ENV['HEROKU']
+        Rails.application.credentials.github[:heroku][:client_secret]
+      when ENV['WWWDOMAIN']
+        Rails.application.credentials.github[:www_domain][:client_secret]
+      when ENV['DOMAIN']
+        Rails.application.credentials.github[:domain][:client_secret]
+      else
+        Rails.application.credentials.github[:heroku][:client_secret]
+      end
+    }.call.to_s    
     provider :github, client_id, client_secret
   end
 end
-
-# CSRF対策の設定
-OmniAuth.config.allowed_request_methods = [:post, :get]
