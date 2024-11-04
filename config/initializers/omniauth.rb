da321d3d58219b02482453530f4e56951df1fb3d
Dingu-suke <e2310000rubik@gmail.com>
2024-11-04 10:14:38 +0900

    omniaut🐛

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,18 +3,36 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
     provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
   else
-    # request.hostに基づいて適切な認証情報を取得
-    creds = case request.host
-            when ENV['HEROKU']
-              Rails.application.credentials.github.heroku
-            when ENV['WWWDOMAIN']
-              Rails.application.credentials.github.www_domain
-            when ENV['DOMAIN']
-              Rails.application.credentials.github.domain
-            end
+    # client_idとclient_secretを個別に取得するlambdaを用意
+    client_id = lambda { |env|
+      request = Rack::Request.new(env)
+      creds = case request.host
+              when ENV['HEROKU']
+                Rails.application.credentials.github.heroku
+              when ENV['WWWDOMAIN']
+                Rails.application.credentials.github.www_domain
+              when ENV['DOMAIN']
+                Rails.application.credentials.github.domain
+              end
+      Rails.logger.info "Host: #{request.host}"
+      creds&.client_id
+    }
 
-    # 配列ではなく、直接2つの引数として渡す
-    provider :github, creds.client_id, creds.client_secret
+    client_secret = lambda { |env|
+      request = Rack::Request.new(env)
+      creds = case request.host
+              when ENV['HEROKU']
+                Rails.application.credentials.github.heroku
+              when ENV['WWWDOMAIN']
+                Rails.application.credentials.github.www_domain
+              when ENV['DOMAIN']
+                Rails.application.credentials.github.domain
+              end
+      creds&.client_secret
+    }
+
+    # 個別のlambdaを引数として渡す
+    provider :github, client_id, client_secret
   end
 end
 
