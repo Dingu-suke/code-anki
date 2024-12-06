b7edf9e1d2efbaf5a659f9c4b9c42e7ca9415ade
Dingu-suke <e2310000rubik@gmail.com>
2024-12-07 05:23:54 +0900

    useContextを使用する形に変更

--- app/javascript/react/components/drill/DrillContent.jsx
+++ app/javascript/react/components/drill/DrillContent.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Markdown from "react-markdown";
-import { useRunCode } from "../../hooks/useRunCode";
-import Output from "../runCodeEditorDaisyUI/runButton&Output/Output";
-import { EditorAndAnswer, EditorForAnswer, EditorForUser } from "../card/EditorAndAnswer";
 import { EditorProvider } from '../../context/EditorProvider';
-
+import { useRunCode } from "../../hooks/useRunCode";
+import { activeTabClassOrange, inactiveTabClassOrange, tabClass } from "../../tabStylesAndFunc/styleClass";
+import { DiffEditorContent, EditorForAnswer, EditorForUser } from "../card/EditorAndAnswer";
+import Output from "../runCodeEditorDaisyUI/runButton-Output/Output";
 
 export const DrillContennt = ({ previewCardList, card, setPreviewCard, moveToNextCard, moveToPreviousCard}) => {
   const [currentCardId, setCurrentCardId] = useState(card?.id)
-  const [bluredCards, setBluredCards] = useState(() => {
+  const [activeTab,     setActiveTab]     = useState('cardbody')
+  const [bluredCards  , setBluredCards]   = useState(() => {
     if (!previewCardList) return {};
     
     // 全てのカードのIDに対して 初期設定 true (ぼかす)
@@ -36,6 +37,10 @@ export const DrillContennt = ({ previewCardList, card, setPreviewCard, moveToNex
       ...prev,
       [cardId]: !prev[cardId]
     }));
+  };
+
+  const handleTabChange = (tabName, updateFunc) => {
+    updateFunc(tabName);
   };
 
   const {
@@ -80,11 +85,15 @@ export const DrillContennt = ({ previewCardList, card, setPreviewCard, moveToNex
     setActiveOutput('answer');
   };
 
+  useEffect(() => {
+    console.log("activeOutput", activeOutput);
+  }, [activeOutput])
+
   return (
     <EditorProvider
                 card={card}
-                runUserCode={runUserCode}
-                runAnswerCode={runAnswerCode}
+                runUserCode={handleRunUserCode}
+                runAnswerCode={handleRunAnswerCode}
                 userIsLoading={userIsLoading}
                 answerIsLoading={answerIsLoading}
                 userEditorRef={userEditorRef}
@@ -98,55 +107,110 @@ export const DrillContennt = ({ previewCardList, card, setPreviewCard, moveToNex
               >
       <div className=" bg-gray-950">
         <br />
-        <div className="grid grid-cols-6 grid-rows-2 gap-4 mb-4">
-            <div className="col-span-3 row-span-2">
-              <div className="border border-slate-600 bg-stone-950 text-cyan-50 rounded overflow-hidden">
-                <div className="bg-slate-800 px-4 py-2 font-semibold">
+        <div className=" gap-4 mb-4">
+          <div className="">
+            <div className="w-full">
+              <div role="tablist" className="flex border-b border-yellow-800">
+                <button
+                role="tab"
+                  className={`${tabClass} ${activeTab === 'cardbody' ? activeTabClassOrange : inactiveTabClassOrange} cursor-auto h-7 text-sm`}
+                  onClick={() => handleTabChange('cardbody', setActiveTab)}
+                  aria-selected={activeTab === 'cardbody'}
+                  aria-controls="cardbody-panel"
+                >
                   問題文
+                </button>
+                <button
+                  role="tab"
+                  className={`${tabClass} ${activeTab === 'output' ? activeTabClassOrange : inactiveTabClassOrange} cursor-auto h-7 text-sm `}
+                  onClick={() => handleTabChange('output', setActiveTab)}
+                  aria-selected={activeTab === 'output'}
+                  aria-controls="output-panel"
+                >
+                  出力
+                </button>
+                <button
+                  role="tab"
+                  className={`${tabClass} ${activeTab === 'diff' ? activeTabClassOrange : inactiveTabClassOrange} cursor-auto h-7 text-sm `}
+                  onClick={() => handleTabChange('diff', setActiveTab)}
+                  aria-selected={activeTab === 'diff'}
+                  aria-controls="diff-panel"
+                >
+                  差分
+                </button>
+              </div>
+
+              {/* 問題文タブで表示する内容 */}
+              <div className="bg-slate-950 border-x border-b border-yellow-800 rounded-b-md">
+                <div
+                  role="tabpanel"
+                  id="cardbody-panel"
+                  className={`p-6 ${activeTab === 'cardbody' ? '' : 'hidden'}`}
+                >
+                  <div className="grid grid-cols-2 grid-rows-2 gap-4">
+                    <div className="col-start-1 col-span-1 row-start-1 row-span-2">
+                      <div className="border border-slate-600 bg-stone-950 text-cyan-50 rounded overflow-hidden h-full">
+                        <div className="bg-slate-800 px-4 py-2 font-semibold">
+                          {card?.title}
+                        </div>
+                        <div className="p-4 h-overflow-auto h-[calc(100%-2.5rem)]">
+                          <Markdown>{card?.body}</Markdown>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-start-2 col-span-1 row-start-1 row-span-1">
+                      <EditorForUser /> {/* EditorProvider useContex を使った呼び出し */}
+                    </div>
+                    <div className="col-start-2 col-span-1 row-start-2 row-span-1">
+                      <EditorForAnswer /> {/* EditorProvider useContext を使った呼び出し*/}
+                    </div>
+                  </div>
                 </div>
-                <div className="p-4 h-[calc(65vh-2rem)] overflow-auto">
-                  <Markdown>{card?.body}</Markdown>
+
+                {/* 出力タブタブで表示する内容 */}
+                <div
+                  role="tabpanel"
+                  id="output-panel"
+                  className={`p-6 ${activeTab === 'output' ? '' : 'hidden'}`}
+                >
+                  <div className="grid grid-cols-2 grid-rows-2 gap-4">
+                    <div className="col-start-1 col-span-1 row-start-1 row-span-2">
+                      <Output
+                          editorRef={activeOutput === 'user' ? userEditorRef : answerEditorRef}
+                          language={activeOutput === 'user' ? userLanguage : answerLanguage}
+                          output={activeOutput === 'user' ? userOutput : answerOutput}
+                          setOutput={activeOutput === 'user' ? setUserOutput : setAnswerOutput}
+                          isError={activeOutput === 'user' ? userIsError : answerIsError}
+                          setIsError={activeOutput === 'user' ? setUserIsError : setAnswerIsError}
+                          message={`Check the Codes here ▷`}
+                          distinguishText={`Output Of ${activeOutput === "user" ? " Your" : "Answer's"} Code`}
+                          activeOutput={activeOutput}
+                        />
+                    </div>
+                    <div className="col-start-2 col-span-1 row-start-1 row-span-1">
+                      <EditorForUser /> {/* EditorProvider useContex を使った呼び出し */}
+                    </div>
+                    <div className="col-start-2 col-span-1 row-start-2 row-span-1">
+                      <EditorForAnswer /> {/* EditorProvider useContext を使った呼び出し*/}
+                    </div>
+                  </div>
+                </div>
+                <div
+                  role="diff"
+                  id="diff-panel"
+                  className={`p-6 ${activeTab === 'diff' ? '' : 'hidden'}`}
+                >
+                  <DiffEditorContent />
                 </div>
               </div>
-            </div>
-            <div className="col-start-4 col-span-3 row-span-1">
-                <div>
-                  <div className="pb-1">
-                    <EditorForUser /> {/* EditorProvider から呼び出し */}
-                  </div>
-                  <div className="pt-1">
-                    <EditorForAnswer /> {/* EditorProvider から呼び出し*/}
-                  </div>
-                </div>
-            </div>
-            <div className="col-start-4 col-span-3 row-span-1 flex flex-col">
-              {/* <div className="border border-slate-600 hover:border-cyan-600 hover:text-cyan-50 text-blue-950 mb-2 rounded shadow flex-grow overflow-auto">
-                <div className="bg-slate-800 text-cyan-100 px-4 py-2 font-semibold">
-                  備考･メモ
-                </div>
-                <div className="px-4 py-4">
-                  <Markdown>{card?.remarks}</Markdown>
-                </div>
-              </div> */}
-                <Output
-                  editorRef={activeOutput === 'user' ? userEditorRef : answerEditorRef}
-                  language={activeOutput === 'user' ? userLanguage : answerLanguage}
-                  output={activeOutput === 'user' ? userOutput : answerOutput}
-                  setOutput={activeOutput === 'user' ? setUserOutput : setAnswerOutput}
-                  isError={activeOutput === 'user' ? userIsError : answerIsError}
-                  setIsError={activeOutput === 'user' ? setUserIsError : setAnswerIsError}
-                  message={`Check the Codes here ▷`}
-                  distinguishText={`Output Of ${activeOutput === "user" ? " Your" : "Answer's"} Code`}
-                  activeOutput={activeOutput}
-                  height={outputHeight}
-                  outputHeight={outputHeight}
-                  setOutputHeight={setOutputHeight}
-                />
-            </div>
-            <div className="col-span-6">
-            </div>
           </div>
+          </div>
+          <div className="">
         </div>
-      </EditorProvider>
+        <div className="col-span-6">
+        </div>
+      </div>
+    </div>
+  </EditorProvider>
   );
 };
