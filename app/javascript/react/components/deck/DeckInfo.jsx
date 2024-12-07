10b03d41e4d1e33c33fdb5bb889875586b1126a1
Dingu-suke <e2310000rubik@gmail.com>
2024-12-08 04:21:56 +0900

    インデントを修正

--- app/javascript/react/components/deck/DeckInfo.jsx
+++ app/javascript/react/components/deck/DeckInfo.jsx
@@ -129,11 +129,10 @@ export const DeckInfo = () => {
   return (
     <div>
         {toast.show && <Toast message={toast.message} type={toast.type} />}
-      <div className="grid grid-cols-6">
-        
-          <div className="col-start-1 col-span-1 pr-2">
-            <SelectedDeckDisplay selectedDeck={selectedDeck}/>
-          </div>        
+      <div className="grid grid-cols-6">        
+        <div className="col-start-1 col-span-1 pr-2">
+          <SelectedDeckDisplay selectedDeck={selectedDeck}/>
+        </div>        
         <div className="col-start-2 col-span-4">
           <PreviewCardList checkedCards={checkedCards} setCheckedCards={setCheckedCards} previewCard={previewCard} setPreviewCard={setPreviewCard} selectedDeck={selectedDeck} />
         </div>
